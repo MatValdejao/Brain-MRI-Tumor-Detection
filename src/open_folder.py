93fb25a757852725a7ee456dfa2ecb61b5179cff
MatValdejao <matvaldejao123@gmail.com>
2026-04-02 15:35:40 -0400

    files extracted accordingly locally, need to check when working within colab for GPU, although only thing that change is extraction and dataset saved path

--- src/open_folder.py
+++ src/open_folder.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c91238ecd54f577660d8680809513a64f5205b026a4b8b77adfb38fde444ffb
-size 1185
+oid sha256:fa1a18f54a6ef8fb57656e44682b579c3efb650a0d1e179f8407246800d2a7e3
+size 1222
