41635289d6ea27d97d46ae4d745896107b85e075
MatValdejao <matvaldejao123@gmail.com>
2026-04-02 18:36:53 -0400

    changed

--- src/open_folder.py
+++ src/open_folder.py
@@ -1,3 +1,39 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b53ca072e6436f39ae753eb6c43938f0598a2b6dcf336ae0c44c622bf3a65872
-size 1258
+# import package and dataset
+import os
+from PIL import Image
+import numpy as np
+
+
+# for loop to open images, loop through each folder name
+def open_folder(path: str):
+    # set image size to 128 pixels
+    image_size = 128
+
+    # initialize a list for images and labels
+    image_list = []
+    image_labels = []
+
+    for folder_name, label in [('glioma', 'glioma'), ('meningioma', 'meningioma'), ('notumor', 'notumor'), ('pituitary', 'pituitary')]:
+        # folder path definition
+        folder_path = os.path.join(path, folder_name)
+
+        # path for each image, remove thumbs.db element
+        image_path = [f for f in os.listdir(folder_path) if f.lower() != 'thumbs.db']
+        # print(image_path)
+
+        # iterate through images and indexes
+        for the_image in image_path:
+            # print(the_image)
+
+            # iterating image variable
+            image = Image.open(os.path.join(folder_path, the_image))
+            # print(image)
+            image = image.convert('RGB')
+
+            # resize images and add them to list dataset
+            image = image.resize((image_size, image_size))
+            image_list.append(np.array(image))
+            image_labels.append(label)
+
+
+    return np.array(image_list), np.array(image_labels)
