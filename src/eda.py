5f3cb3f60dbcbba68f11297e40e007f5a82ee0eb
MatValdejao <matvaldejao123@gmail.com>
2026-04-02 16:27:52 -0400

    will randomize eda graphing to vary arrange plots plotting

--- src/eda.py
+++ src/eda.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bfebee74e1b4e4e10d40decea926f4af1171e6bb06c4080c9b21e1a34b4b4865
-size 1550
+oid sha256:73eca02618670f387fa64e16eadfc4c56a550226f6796e4dc60a453e44451ec6
+size 914
--- src/open_folder.py
+++ src/open_folder.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa1a18f54a6ef8fb57656e44682b579c3efb650a0d1e179f8407246800d2a7e3
-size 1222
+oid sha256:b53ca072e6436f39ae753eb6c43938f0598a2b6dcf336ae0c44c622bf3a65872
+size 1258
